f57d30862a95c69b10da9867e879df3c2d35d53c
lluckymou <llucmou@gmail.com>
2026-02-23 12:01:35 -0300

    Local marked.ks

--- rcb/settings.py
+++ rcb/settings.py
@@ -50,6 +50,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "app.context_processors.carregar_globais",
+                'journal_mode': 'WAL',
             ],
         },
     },
